b8f86306be9d2a4f09f56bb603c9c7fd0523f30d
komed3 <pk97@outlook.de>
2026-03-25 00:19:13 +0100

    define types

--- src/path.ts
+++ src/path.ts
@@ -1,1 +1,12 @@
+export type PathToken = string | number;
+
+export interface CompiledPath {
+    tokens: PathToken[];
+}
+
+export interface PathOptions {
+    cache?: boolean;
+    maxCacheSize?: number;
+}
+
 export class Path {}
