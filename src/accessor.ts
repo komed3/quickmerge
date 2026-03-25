8c9a5ff46bc9a5eb02ae4e663ceaa95a987d8c5f
komed3 <pk97@outlook.de>
2026-03-25 11:18:02 +0100

    implement private walker

--- src/accessor.ts
+++ src/accessor.ts
@@ -12,6 +12,27 @@ export class Accessor {
         return this.path.normalize( path ).tokens;
     }
 
+    private walk ( obj: any, tokens: PathToken[], create: boolean ) : { parent: any; key: PathToken; value: any } | undefined {
+        let cur = obj;
+
+        for ( let i = 0; i < tokens.length - 1; i++ ) {
+            const key = tokens[ i ];
+            let next = cur?.[ key ];
+
+            if ( next == null ) {
+                if ( ! create ) return;
+
+                next = typeof tokens[ i + 1 ] === 'number' ? [] : {};
+                cur[ key ] = next;
+            }
+
+            cur = next;
+        }
+
+        const lastKey = tokens[ tokens.length - 1 ];
+        return { parent: cur, key: lastKey, value: cur?.[ lastKey ] };
+    }
+
 }
 
 const defaultAccessor = new Accessor ();
