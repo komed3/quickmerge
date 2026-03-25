fb7316706c8547ddb5c2586419addaebec296795
komed3 <pk97@outlook.de>
2026-03-25 19:19:58 +0100

    implement a factory function

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * QuickMerge is a fast and flexible object merging library.
+ * 
+ * Use the factory function to create a new instance of QuickMerge or
+ * import the individual classes to create your own instances.
+ * 
+ * @author Paul Köhler
+ * @license MIT
+ */
+
+import { Accessor } from './accessor';
+import { Merger, type MergeOptions } from './merger';
+import { Path } from './path';
+
+export * from './accessor';
+export * from './merger';
+export * from './path';
+
+
+/**
+ * Creates a new instance of QuickMerge.
+ * 
+ * @param {MergeOptions} options - Configuration options for the QuickMerge instance.
+ * @returns {QuickMerge} A new instance of QuickMerge.
+ */
+export const factory = ( options?: MergeOptions ) => ( {
+    accessor: new Accessor( options?.pathOptions ),
+    merger: new Merger( options ),
+    path: new Path( options?.pathOptions )
+} );
