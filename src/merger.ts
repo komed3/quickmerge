4cfc5ca06d133c81888a026ac82d8b35c7d8eeb3
komed3 <pk97@outlook.de>
2026-03-25 15:27:55 +0100

    add Merger class

--- src/merger.ts
+++ src/merger.ts
@@ -1,3 +1,5 @@
+import { Path, PathOptions } from './path';
+
 export const enum ArrayMode {
     Replace = 'replace',
     Concat = 'concat',
@@ -10,4 +12,18 @@ export interface MergeOptions {
     protect?: boolean;
     mergeUndefined?: boolean;
     arrayMode?: ArrayMode | ( ( target: any[], source: any[] ) => any[] );
+    pathOptions?: PathOptions;
+}
+
+export class Merger {
+
+    private readonly deep: boolean;
+    private readonly protect: boolean;
+    private readonly mergeUndefined: boolean;
+
+    private readonly mergeArray: ( a: any[], b: any[] ) => any[];
+    private readonly path: Path;
+
+    constructor ( options: MergeOptions ) {}
+
 }
