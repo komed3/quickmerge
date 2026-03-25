b69ee5878f70494f17d609f12e0c57dbd8a9f892
komed3 <pk97@outlook.de>
2026-03-25 11:16:25 +0100

    implement private normalize method

--- src/accessor.ts
+++ src/accessor.ts
@@ -1,11 +1,15 @@
-import { CompiledPath, Path, PathOptions } from './path';
+import { CompiledPath, Path, PathOptions, PathToken } from './path';
 
 export class Accessor {
 
     private readonly path: Path;
 
     constructor ( options: PathOptions = {} ) {
-        this.path = new Path( options );
+        this.path = new Path ( options );
+    }
+
+    private normalize ( path: string | CompiledPath ) : PathToken[] {
+        return this.path.normalize( path ).tokens;
     }
 
 }
