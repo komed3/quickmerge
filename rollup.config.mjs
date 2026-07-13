d2f8b67a1ea9f9e3d36260d4c0aac418a0216eef
komed3 <webmaster@komed3.de>
2026-07-13 20:16:24 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,35 +6,35 @@ import cleanup from 'rollup-plugin-cleanup';
 import prettier from 'rollup-plugin-prettier';
 
 export default {
-    input: 'src/index.ts',
-    plugins: [
-        resolve( { extensions: [ '.js', '.ts' ] } ), commonjs(),
-        typescript( { tsconfig: 'tsconfig.json', compilerOptions: { declaration: false } } ),
-        cleanup( { comments: 'istanbul', extensions: [ 'js', 'ts' ] } ),
-        prettier( {
-            parser: 'babel', tabWidth: 2, bracketSpacing: true, bracketSameLine: true, singleQuote: true,
-            jsxSingleQuote: true, trailingComma: 'none', objectWrap: 'collapse'
-        } )
-    ],
-    output: [ {
-        dir: 'dist',
-        format: 'cjs',
-        entryFileNames: '[name].cjs',
-        preserveModules: true,
-        exports: 'named'
-    }, {
-        dir: 'dist',
-        format: 'es',
-        entryFileNames: '[name].mjs',
-        preserveModules: true
-    }, {
-        file: 'dist/bundle.umd.js',
-        format: 'umd',
-        name: 'deepmerge'
-    }, {
-        file: 'dist/bundle.umd.min.js',
-        format: 'umd',
-        name: 'deepmerge',
-        plugins: [ terser() ]
-    } ]
+  input: 'src/index.ts',
+  plugins: [
+    resolve( { extensions: [ '.js', '.ts' ] } ), commonjs(),
+    typescript( { tsconfig: 'tsconfig.json', compilerOptions: { declaration: false } } ),
+    cleanup( { comments: 'istanbul', extensions: [ 'js', 'ts' ] } ),
+    prettier( {
+      parser: 'babel', tabWidth: 2, bracketSpacing: true, bracketSameLine: true, singleQuote: true,
+      jsxSingleQuote: true, trailingComma: 'none', objectWrap: 'collapse'
+    } )
+  ],
+  output: [ {
+    dir: 'dist',
+    format: 'cjs',
+    entryFileNames: '[name].cjs',
+    preserveModules: true,
+    exports: 'named'
+  }, {
+    dir: 'dist',
+    format: 'es',
+    entryFileNames: '[name].mjs',
+    preserveModules: true
+  }, {
+    file: 'dist/bundle.umd.js',
+    format: 'umd',
+    name: 'deepmerge'
+  }, {
+    file: 'dist/bundle.umd.min.js',
+    format: 'umd',
+    name: 'deepmerge',
+    plugins: [ terser() ]
+  } ]
 };
